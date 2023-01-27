e18a173361d2fd80bf666d1699fa9585396e3c32
dsjordao <jordaodaniel@hotmail.com>
2023-01-27 07:39:16 -0300

    first commit

---
+++ src/app/core/components/auth/auth.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+// Routing
+import { AuthRoutingModule } from './auth-routing.module';
+
+// Pages Components
+import { SignComponent } from './pages/sign/sign.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    SignComponent
+  ],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    ReactiveFormsModule   
+  ]
+})
+export class AuthModule { }
